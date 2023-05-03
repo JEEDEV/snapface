9e290d9e804fab6f58df026f741f8c111a87efe0
JEEDEV <bilel.frikha@yahoo.fr>
2023-05-03 19:52:44 +0200

    add dynamic style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,31 +7,31 @@ import { FaceSnap } from './models/face-snap.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-  mySnap!:FaceSnap;
-  myOtherSnap!:FaceSnap;
-  myLastSnap!:FaceSnap;
+  faceSnaps!:FaceSnap[];
   ngOnInit(): void {
-    this.mySnap= {
-                                title:'Archibald',
-                                description:'Mon meilleur ami depuis tout petit !',
-                                imageUrl:'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-                                createdDate:new Date(),
-                                location:'Paris',
-                                snaps:0
-                              };
-    this.myOtherSnap= {
-                                  title:'Three Rock Mountain',
-                                  description:'Un endroit magnifique pour les rendonnées',
-                                  imageUrl:'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/1920px-Three_Rock_Mountain_Southern_Tor.jpg',
-                                  createdDate:new Date(),
-                                  snaps:0
-                                };
-    this.myLastSnap= {
-                                  title:'Un bon repas',
-                                  description:'Mmmh que c\'est bon !',
-                                  imageUrl:'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
-                                  createdDate:new Date(),
-                                  snaps:0
-                                };                                                            
+    this.faceSnaps = [
+      {
+        title:'Archibald',
+        description:'Mon meilleur ami depuis tout petit !',
+        imageUrl:'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        createdDate:new Date(),
+        location:'Paris',
+        snaps:255
+      },
+      {
+        title:'Three Rock Mountain',
+        description:'Un endroit magnifique pour les rendonnées',
+        imageUrl:'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/1920px-Three_Rock_Mountain_Southern_Tor.jpg',
+        createdDate:new Date(),
+        snaps:0
+      },
+      {
+        title:'Un bon repas',
+        description:'Mmmh que c\'est bon !',
+        imageUrl:'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+        createdDate:new Date(),
+        snaps:0
+      }
+    ];                                                         
   } 
 }
